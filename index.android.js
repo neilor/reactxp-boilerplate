49dc4fb028ba79bb91952c4eaf44e85cfcd9ccad
Neilor Caldeira <neilorcaldeira@gmail.com>
2018-07-02 12:23:04 -0300

    init: first commit inherited from reactxp/samples/TodoList

---
+++ index.android.js
@@ -0,0 +1,13 @@
+/**
+* index.android.js
+* Copyright: Microsoft 2018
+*
+* Javascript main entry point for native app.
+*/
+
+var AppBootstrapperNative = require('./temp/android/rnapp/js/app/AppBootstrapperNative.js');
+
+// This prevents bundlers/optimizers from stripping out the import above.
+if (AppBootstrapperNative) {
+    console.log('App started');
+}
